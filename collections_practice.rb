6dc8b21a63398382797f66213ed3e7556b714cc8
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-02 04:19:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+ array.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(a)
+a[1], a[2] = a[2], a[1]
+return a
+end
+
+def reverse_array(a)
+  a.reverse 
+end
+
+def kesha_maker(array)
+  new_array[]
+  array.each do |a| 
+  new_array << ("bl$ke","as$ley","sc$tt")
+end
+new_array
+end
+  
